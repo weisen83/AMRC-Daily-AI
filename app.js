577e1b88bba60f758bbdd928c7786433790ca9b1
weisen83 <rchen5061@gmail.com>
2026-07-07 21:03:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-fetch('market.json').then(r=>r.json()).then(d=>{document.getElementById('date').innerText=d.date;let h='<div class="card"><h2>'+d.condition+'</h2></div>';d.markets.forEach(m=>{h+=`<div class="card"><h2>${m.name}</h2><pre>${m.structure}</pre></div>`});document.getElementById('content').innerHTML=h;});
+fetch('./market.json')
+.then(response => response.json())
+.then(data => {
+
+    document.getElementById("date").innerText = data.date;
+
+    let html = `
+    <div class="card">
+        <h2>${data.condition}</h2>
+    </div>
+    `;
+
+    data.markets.forEach(market => {
+
+        html += `
+        <div class="card">
+            <h2>${market.name}</h2>
+            <pre>${market.structure}</pre>
+            <button onclick="copyText('${market.name}')">
+            複製
+            </button>
+        </div>
+        `;
+
+    });
+
+    document.getElementById("content").innerHTML = html;
+
+})
+.catch(error=>{
+    document.getElementById("content").innerHTML =
+    "<h2>資料載入失敗</h2>";
+    console.log(error);
+});
+
+
+function copyText(text){
+    navigator.clipboard.writeText(text);
+    alert("已複製");
+}
