b6058a100997578c4d0c0b24a5aa2e0c29bdf45d
weisen83 <rchen5061@gmail.com>
2026-07-08 21:44:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,43 +13,43 @@ console.log(data)；
     </div>
     `;
 
+ data["市場"].forEach(market => {
 
-    data【"市場"】.forEach(market => {
-
-        html += `
+html += `
 <div class="card">
 
-<h2>${market.【"商品名"】}</h2>
+<h2>${market["商品名"]}</h2>
 
 <h3>📌 Market Structure</h3>
-<p>${market【"市場結構"】}</p>
+<p>${market["市場結構"]}</p>
 
 <h3>💧 Liquidity</h3>
-<p>${market【"流動性"】}</p>
+<p>${market["流動性"]}</p>
 
 <h3>📊 Order Flow</h3>
-<p>${market【"訂單流"】}</p>
+<p>${market["訂單流"]}</p>
 
 <h3>🎯 Trading Plan</h3>
-<p>${market【"交易計畫"】}</p>
+<p>${market["交易計畫"]}</p>
+
 
 <button onclick="copyText(
-\`${market【"市場結構"】}
+\`${market["市場結構"]}
 
-${market【"流動性"】}
+${market["流動性"]}
 
-${market【"訂單流"】}
+${market["訂單流"]}
 
-${market【"交易計畫"】}\`
+${market["交易計畫"]}\`
 )">
 複製分析
 </button>
 
+
 </div>
 `;
 
-
-    });
+});
 
 
     document.getElementById("content").innerHTML = html;
