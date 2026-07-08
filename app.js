c6ca83c9e3d0f1c3d356c0df197a46e08409c91b
weisen83 <rchen5061@gmail.com>
2026-07-08 21:56:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,56 +4,11 @@ fetch("./market.json")
 
 console.log(data);
 
-document.getElementById("date").innerText = data.date;
-
-let html = "";
-
-    html += `
-    <div class="card">
-        <h2>${data.condition}</h2>
-    </div>
-    `;
-
-data["市場"].forEach(market => {
-
-html += `
+document.getElementById("content").innerHTML =
+`
 <div class="card">
-
-<h2>${market["商品名"]}</h2>
-
-<h3>📌 Market Structure</h3>
-<p>${market["市場結構"]}</p>
-
-<h3>💧 Liquidity</h3>
-<p>${market["流動性"]}</p>
-
-<h3>📊 Order Flow</h3>
-<p>${market["訂單流"]}</p>
-
-<h3>🎯 Trading Plan</h3>
-<p>${market["交易計畫"]}</p>
-
+<h2>${data.condition}</h2>
 </div>
 `;
 
 });
-    document.getElementById("content").innerHTML = html;
-
-})
-.catch(error => {
-
-    console.log(error);
-
-    document.getElementById("content").innerHTML =
-    "<h2>資料載入失敗</h2>";
-
-});
-
-
-function copyText(text){
-
-navigator.clipboard.writeText(text);
-
-alert("已複製");
-
-}
